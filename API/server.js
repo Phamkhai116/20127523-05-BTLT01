dd874d511bd73b2055569346909955500bdb5676
Phamkhai116 <75026919+Phamkhai116@users.noreply.github.com>
2022-11-17 15:08:00 +0700

    Set header with Json

--- API/server.js
+++ API/server.js
@@ -1,9 +1,17 @@
 const http = require('http');
 
+const COURSES = [
+    {id : 1, name: 'NodeJS'},
+    {id : 2, name: 'ReacJS'},
+]
+
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-type' , 'text/plain')
-    res.write('Hello word');
-    res.end();
+    res.setHeader('Content-type' , 'application/json')
+
+    res.end(JSON.stringify({
+        success : true,
+        data : COURSES
+    }));
 })
 
 const PORT = 3000
