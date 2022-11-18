1218d396bae32fc15421c4ec02368280dbd6e3db
Phamkhai116 <75026919+Phamkhai116@users.noreply.github.com>
2022-11-18 13:02:44 +0700

    Get

--- API/server.js
+++ API/server.js
@@ -1,11 +1,12 @@
-const http = require('http');
-
+//const http = require('http');
+const express = require('express')
+const app = express()
 const COURSES = [
     {id : 1, name: 'NodeJS'},
     {id : 2, name: 'ReacJS'},
 ]
 
-const server = http.createServer((req, res) => {
+/*const server = http.createServer((req, res) => {
     res.writeHead(404, {
         'Content-type' : 'application/json',
         'X-Powered-By' : 'Node.js'
@@ -16,7 +17,25 @@ const server = http.createServer((req, res) => {
         error : 'NOT FOUND',  
         data : null
     }));
-})
+})*/
 
-const PORT = 3000
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//const PORT = 3000
+
+app.get('/', (req, res) => {
+    res.send('Khải đẹp trai');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send(COURSES);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const COURSE = COURSES.find(COURSES => COURSES.id === parseInt(req.params.id))
+    if(!COURSE)
+        res.status(404).send('ID không tồn tại')
+    res.send(COURSE);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port  ${PORT}`));
+//server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
